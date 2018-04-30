0ae7123657eff0d060cb808e4bf74b80834c8372
ishan <ishan.office@gmail.com>
2018-04-30 17:32:23 +0530

    Added code for web entities

--- doc_iden.py
+++ doc_iden.py
@@ -6,8 +6,47 @@ import os
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
-
 from google.oauth2 import service_account
+
+def print_label_annotations(annotations):
+	print('Labels:')
+
+	if annotations:
+		for label in annotations:
+			print(label.description)
+
+
+def print_web_annotations(annotations):
+    print('Web:')
+
+    if annotations.pages_with_matching_images:
+        print('\n{} Pages with matching images retrieved'.format(
+            len(annotations.pages_with_matching_images)))
+
+        for page in annotations.pages_with_matching_images:
+            print('Url   : {}'.format(page.url))
+
+    if annotations.full_matching_images:
+        print ('\n{} Full Matches found: '.format(
+               len(annotations.full_matching_images)))
+
+        for image in annotations.full_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if annotations.partial_matching_images:
+        print ('\n{} Partial Matches found: '.format(
+               len(annotations.partial_matching_images)))
+
+        for image in annotations.partial_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if annotations.web_entities:
+        print ('\n{} Web entities found: '.format(
+            len(annotations.web_entities)))
+
+        for entity in annotations.web_entities:
+            print('Score      : {}'.format(entity.score))
+            print('Description: {}'.format(entity.description))
 
 credentials = service_account.Credentials.from_service_account_file('key.json')
 
@@ -25,10 +64,8 @@ with io.open(file_name, 'rb') as image_file:
 image = types.Image(content=content)
 
 # Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+label_annotations 	= client.label_detection(image=image).label_annotations
+web_detection		= client.web_detection(image=image).web_detection
 
-print('Labels:')
-for label in labels:
-    print(label.description)
-
+print_label_annotations(label_annotations)
+print_web_annotations(web_detection)
